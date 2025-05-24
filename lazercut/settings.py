7770826fbb15d51ca06d42170ea5d7c2df9d0cb5
Akram <akrambenghanem76@gmail.com>
2025-05-24 03:26:34 +0100

    fix: Ensure DATABASE_URL retrieves an empty string by default if not set

--- lazercut/settings.py
+++ lazercut/settings.py
@@ -21,7 +21,7 @@ print(f"DEBUG: {DEBUG}")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DATABASE_URL = os.environ.get('POSTGRES_URL')
+DATABASE_URL = os.environ.get('POSTGRES_URL', '')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
