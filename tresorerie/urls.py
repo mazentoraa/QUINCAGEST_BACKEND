d92eb4d7c868c8461d70a9095d347194d3406115
bachri fatima ezzahra <109394388+Faty131@users.noreply.github.com>
2025-07-18 14:33:05 +0100

    mise a jour

--- tresorerie/urls.py
+++ tresorerie/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import ( KPIView, ScheduleView, TraiteView, PeriodView )
 
 urlpatterns = [
-    path("api/kpis/", KPIView.as_view()),
-    path("api/schedule/", ScheduleView.as_view(), name="schedule"),
-    path("api/traites/", TraiteView.as_view(), name="traites"),
-    path('api/period/', PeriodView.as_view()),
+    path("kpis/", KPIView.as_view()),
+    path("schedule/", ScheduleView.as_view(), name="schedule"),
+    path("traites/", TraiteView.as_view(), name="traites"),
+    path('period/', PeriodView.as_view()),
 ]
